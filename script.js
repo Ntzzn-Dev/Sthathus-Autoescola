7a402b4d17cce4fb80139eb7f5d798352f5ffecc
Ntzzn-Dev <ntzzn.dev@gmail.com>
2025-04-29 17:08:36 -0300

    Sthathus_v1.5

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function expandQuest(e) {
   const el = e.currentTarget;
   el.classList.toggle("active");
 }
+
+const target = document.getElementById("title-typing");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      target.classList.add("typing");
+      observer.unobserve(target); // remove o observer após a primeira vez
+    }
+  });
+});
+
+observer.observe(target);
